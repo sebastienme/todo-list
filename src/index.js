e0823ef6adbb9fbc1ea206ed9df174f915fc94ca
sebastien <sebastien.meslage@gmail.com>
2023-10-15 10:16:40 -0400

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './style.css';
-import './dom.js';
-import './projects.js';
-import './local.js';
-import { changeDom } from './dom.js';
-
-// add project section to main page at loading
-changeDom.initialiseProjects();
