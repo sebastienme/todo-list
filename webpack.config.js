a6a7418e3b266b80e790445f4854602e23ee9699
sebastien <sebastien.meslage@gmail.com>
2023-10-15 10:14:22 -0400

    change deploy setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -7,12 +6,6 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './dist/index.html'  
-    })
-],
   module: {
     rules: [
       {
